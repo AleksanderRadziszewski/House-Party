f35b7cb6d32014d7cfb47cc0f327de63a67ccae3
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-02-19 10:10:19 +0100

    CreateAPIView

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main 
+from .views import RoomView
+# from . import views
 
 urlpatterns = [
-    path('api/', main ),
+    path('api', RoomView.as_view() ),
 ]
