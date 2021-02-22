d550829696d519afc222b0ec94fcc1b8de134f1a
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-02-22 14:17:43 +0100

    index.html, first rendering whith App component

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('api.urls'))
+    path('home/', include('api.urls')),
+    path('', include('frontend.urls'))
 ]
 
